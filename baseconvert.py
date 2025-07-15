05079138df594e6cf2ff5fbaa7de76f4dace49c7
Kemal cater <kc@caterworld.xyz>
2025-07-14 20:43:47 -0400

    added binDec and updated hexDec

--- baseconvert.py
+++ baseconvert.py
@@ -1,5 +1,4 @@
-hexValue = "4D361"
-hexDec = {"0": 0, 
+hexDecDict = {"0": 0, 
           "1": 1,
           "2": 2,
           "3": 3,
@@ -16,20 +15,26 @@ hexDec = {"0": 0,
           "E": 14,
           "F": 15}
 
-def hexDec(num: str,
-           direction: bool = True):
+def binDec(num: str):
+    sum = 0
+    power = 0
+    for digit in reversed(num):
+        if digit == '1':
+            sum += int(digit) * (2 ** power)
+            power += 1
+        elif digit == '0':
+            sum += 0
+            power += 1
+    return sum
+
+def hexDec(num: str):
+    hexDecDict = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
     if type(num) != str:
         num = str(num)
-    if direction:
-        sum = 0
-        for digit in num:
-        sum += hexDec[digit] * (16 ** (len(num) - num.index(digit) - 1))
-        return sum
-    if not direction:
-        ...
+    sum = 0
+    for digit in num:
+        sum += hexDecDict[digit] * (16 ** (len(num) - num.index(digit) - 1))
+    return sum
 
-
-print(hexDec(hexValue))
-print(hexDec("34"))
-print(hexDec("AC"))
-print(hexDec("5E"))
+print(hexDec("4D361") == 316257)
+print(binDec("1101010101") == 853)
