9b5fe236e1fbacd86d455cf94072e4bb8393451a
Kemal <kc@caterworld.xyz>
2025-12-02 23:24:20 -0500

    Implemented cipher function

--- Caesar/Caesar.java
+++ Caesar/Caesar.java
@@ -1,22 +1,25 @@
 import java.util.*;
 import java.io.*;
 import java.lang.System;
+@SuppressWarnings("unused")
 
 public class Caesar {
     private String chars = "!?.,'_-;:\"()[]{}<>@#$%^&*~`+=/\\|\n\r\t";
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         long startTime = System.nanoTime();
         String plaintext = "Hello, World!";
         compileWords(plaintext.length());
+        System.out.printf("%s milliseconds\n", (System.nanoTime() - startTime) / 1000000);
         String ciphertext = caesarCipher(plaintext, 3, true);
-        System.out.println(System.nanoTime() - startTime);
+        System.out.printf("Ciphertext: %s\n", ciphertext);
+        System.out.printf("%s milliseconds\n", (System.nanoTime() - startTime) / 1000000);
     }
 
-    public ArrayList<String> compileWords() {
+    public static ArrayList<String> compileWords() {
         try{
             ArrayList<String> list = new ArrayList<String>();
-            File wordsFile = new File("WordLists/allWords.txt");
+            File wordsFile = new File("Caesar/WordLists/allWords.txt");
             Scanner input = new Scanner(wordsFile);
             while (input.hasNextLine()){
                 list.add(input.nextLine());
@@ -29,10 +32,10 @@ public class Caesar {
             return null;
         }
     }
-    public ArrayList<String> compileWords(int lenText) {
+    public static ArrayList<String> compileWords(int lenText) {
         try{
             ArrayList<String> list = new ArrayList<String>();
-            File wordsFile = new File("WordLists/allWords.txt");
+            File wordsFile = new File("Caesar/WordLists/allWords.txt");
             Scanner input = new Scanner(wordsFile);
             while (input.hasNextLine()){
                 String next = input.nextLine();
@@ -49,7 +52,33 @@ public class Caesar {
     }
 
     public static String caesarCipher(String text, int shift, boolean encrypt) {
-        ArrayList<String> alphabet = new ArrayList<String>();
-        return "";
+        String[] alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        if (encrypt){
+            String encoded = "";
+            for (int i = 0; i < text.length(); i++) {
+                String glyph = text.substring(i, i + 1);
+                if (!Arrays.asList(alpha).contains(glyph.toLowerCase())) {
+                    encoded += glyph;
+                    continue;
+                }
+                if (glyph.equals(" ")) {
+                    encoded += " ";
+                    continue;
+                }
+                if (glyph.equals(glyph.toLowerCase())) {
+                    encoded += alpha[(Arrays.asList(alpha).indexOf(glyph) + shift) % 26];
+                    continue;
+                }
+                if (glyph.equals(glyph.toUpperCase())) {
+                    encoded += alpha[(Arrays.asList(alpha).indexOf(glyph.toLowerCase()) + shift) % 26].toUpperCase();
+                    continue;
+                }
+            }
+            return encoded;
+        }
+        if (!encrypt){
+            return caesarCipher(text, -shift, true);
+        }
+        return null;
     }
 }
